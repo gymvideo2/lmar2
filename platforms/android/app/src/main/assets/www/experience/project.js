49fd0f0cdbc4e1941d7c8b7fa5f185e1d08dbd3c
gymvideo2 <giammarco@jumpcomm.it>
2018-09-19 16:31:27 +0200

    CIAO
    ciao

---
+++ platforms/android/app/src/main/assets/www/experience/project.js
@@ -0,0 +1,250 @@
+// JSON file describing the AR-experience for offline usage
+var projectJSONOffline =
+{
+    "isCloud": false,
+    "tcId": "5b93cb41f4d01a2ec55bec98",
+    "name": "ciao",
+    "targets": [
+        {
+            "name": "DM0jyf6XkAAWbYO",
+            "augmentations": [
+                {
+                    "type": "ImageDrawable",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/164447846_DM0jyf6XkAAWbYO/5b93cbccbf9abc2ecb5f283d/image.png",
+                    "targetId": "5b93cb5dd8b0762ed11ecd51",
+                    "minSDK": "6.1.0",
+                    "modDat": 1536413129985,
+                    "creDat": 1536412620718,
+                    "properties": {
+                        "name": "icon",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": -0.031,
+                            "y": -0.074,
+                            "z": 0.3
+                        },
+                        "zOrder": 0
+                    },
+                    "fileSize": 7334,
+                    "id": "5b93cbccbf9abc2ecb5f283d"
+                }
+            ]
+        },
+        {
+            "name": "1819 ARGENTA squadra CAOSSI U13 nati nel 2006 - 2007",
+            "augmentations": [
+                {
+                    "type": "ImageDrawable",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/747288858_1819_ARGENTA_squadra_CAOSSI_U13_nati_nel_2006_2007/5b98cf408a82042ebfee5729/image.jpeg",
+                    "targetId": "5b98c5668a82042ebfee55fe",
+                    "minSDK": "6.1.0",
+                    "modDat": 1536741196652,
+                    "creDat": 1536741184503,
+                    "properties": {
+                        "name": "1819 ARGENTA squadra CAOSSI ESORDIENTI nati nel 2007 - 2008",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0.578
+                        },
+                        "zOrder": 0
+                    },
+                    "fileSize": 313803,
+                    "id": "5b98cf408a82042ebfee5729"
+                }
+            ]
+        },
+        {
+            "name": "focus-magnani",
+            "augmentations": [
+                {
+                    "type": "ImageDrawable",
+                    "height": 0.3,
+                    "uri": "augmentation-assets/1493426548_focus_magnani/5b93cf1ef4d01a2ec55beca8/image.jpeg",
+                    "targetId": "5b93cf0cd8b0762ed11ecd59",
+                    "minSDK": "6.1.0",
+                    "modDat": 1536738073248,
+                    "creDat": 1536413470786,
+                    "properties": {
+                        "name": "hqdefault",
+                        "forceNativeBrowser": "false",
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": 0.772,
+                            "y": -0.137,
+                            "z": 0.361
+                        },
+                        "zOrder": 2
+                    },
+                    "fileSize": 18359,
+                    "id": "5b93cf1ef4d01a2ec55beca8"
+                },
+                {
+                    "height": 0.1,
+                    "type": "Label",
+                    "targetId": "5b93cf0cd8b0762ed11ecd59",
+                    "minSDK": "6.1.0",
+                    "modDat": 1536738063539,
+                    "creDat": 1536413571615,
+                    "properties": {
+                        "name": "label_1",
+                        "text": "Lucia Magnani AR Experience!!",
+                        "style": {
+                            "backgroundColor": "rgba(0,0,0,1)",
+                            "fontStyle": "normal",
+                            "textColor": "rgba(255,255,255,1)"
+                        },
+                        "ignoreInAR": false,
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 1,
+                            "y": 1,
+                            "z": 1
+                        },
+                        "translate": {
+                            "x": -0.214,
+                            "y": 0.435,
+                            "z": 0.871
+                        },
+                        "zOrder": 1
+                    },
+                    "fileSize": 0,
+                    "id": "5b93cf83d8b0762ed11ecd5c"
+                },
+                {
+                    "type": "VideoDrawable",
+                    "height": 0.3,
+                    "uri": "http://www.longlifeclinic.it/wp-content/themes/activello/videos/video.mp4",
+                    "targetId": "5b93cf0cd8b0762ed11ecd59",
+                    "externalUri": true,
+                    "firstFrameImgUrlFileSize": 0,
+                    "firstFrameAlphaImgUrlFileSize": 0,
+                    "firstFrameAlphaImgUrl": "augmentation-assets/1493426548_focus_magnani/5b98c3058a82042ebfee55f9/firstFrameAlphaImgUrl.jpg",
+                    "minSDK": "6.1.0",
+                    "modDat": 1536738084007,
+                    "creDat": 1536738053924,
+                    "properties": {
+                        "firstFrameImgUrl": "augmentation-assets/1493426548_focus_magnani/5b98c3058a82042ebfee55f9/firstFrameImg.jpg",
+                        "autoPlay": false,
+                        "autoResume": false,
+                        "endlessLoop": false,
+                        "ignoreInAR": false,
+                        "name": "videodrawable_1",
+                        "opacity": 1,
+                        "rotate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "scale": {
+                            "x": 3,
+                            "y": 3,
+                            "z": 3
+                        },
+                        "translate": {
+                            "x": 0,
+                            "y": 0,
+                            "z": 0.492
+                        },
+                        "videoMode": "overlay",
+                        "zOrder": 0
+                    },
+                    "fileSize": 0,
+                    "id": "5b98c3058a82042ebfee55f9"
+                }
+            ]
+        }
+    ],
+    "type": "ImageTargetCollection",
+    "wtcFiles": [
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 3,
+            "version": "6.0",
+            "creDat": 1536738671096
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 3,
+            "version": "6.1",
+            "creDat": 1536738671096
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 3,
+            "version": "6.2",
+            "creDat": 1536738671096
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 3,
+            "version": "7.0",
+            "creDat": 1536738671096
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 3,
+            "version": "7.1",
+            "creDat": 1536738671096
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 3,
+            "version": "7.2",
+            "creDat": 1536738671096
+        },
+        {
+            "url": "augmentation-trackers/6.0/tracker.wtc",
+            "nrOfTargets": 3,
+            "version": "8.0",
+            "creDat": 1536738671096
+        }
+    ],
+    "settings": {
+        "tracker": {}
+    }
+}
+;
